7ef2c6cf6278b3adaf001b42d0b5a2e8c9269242
Wavum <rl00@gmx.at>
2017-01-06 19:38:22 +0100

    Update GoogleData.js

--- js/Data/GoogleData.js
+++ js/Data/GoogleData.js
@@ -17,7 +17,7 @@ var GoogleData = new function()
 
         var s = document.createElement("script");
 
-        s.src = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D%22http%3A%2F%2Fsuggestqueries.google.com%2Fcomplete%2Fsearch%3Fclient%3Dfirefox%26q%3D" +
+        s.src = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D%22http%3A%2F%2Fsuggestqueries.google.com%2Fcomplete%2Fsearch%3Fclient%3Dfirefox%26q%3D" +
                 encodeURIComponent(encodeURIComponent(value)) +
                 "%22%20&format=json&callback=GoogleData.getSearchSuggestions." + id;
         s.id = "searchSuggestionsQuery" + id;
